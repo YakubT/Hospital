3ee9aea9980c2e7cad8d35fac46238c998280c5a
YakubT <goldyakub@gmail.com>
2022-09-12 18:34:38 +0300

    I created project and User model

---
+++ src/main/java/com/solvd/hospital/models/User.java
@@ -0,0 +1,68 @@
+package com.solvd.hospital.models;
+
+public class User {
+
+    private long id;
+
+    private String login;
+
+    private String password;
+
+    private String name;
+
+    private String middleName;
+
+    private String surname;
+
+    public User() {
+
+    }
+
+    public User(long id, String login, String password, String name, String middleName, String surname) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.middleName = middleName;
+        this.surname = surname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return  surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
