b016455c0441900b0ed5c69eb82138764b75616b
YakubT <goldyakub@gmail.com>
2022-09-13 15:55:59 +0300

    Created MedicalField model

---
+++ src/main/java/com/solvd/hospital/models/MedicalField.java
@@ -0,0 +1,33 @@
+package com.solvd.hospital.models;
+
+public class MedicalField {
+
+    long id;
+
+    String name;
+
+    public MedicalField() {
+
+    }
+
+    public MedicalField(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
